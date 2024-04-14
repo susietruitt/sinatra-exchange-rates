70958ff34c71446d9777957c2a7a1fc1a00cf67f
susietruitt <91689219+susietruitt@users.noreply.github.com>
2024-04-14 21:32:15 +0000

    commitiing again

--- app.rb
+++ app.rb
@@ -1,12 +1,20 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'http'
+require 'json'
 @er_key = ENV.fetch("ERKEY")
 
 #api_url = "https://api.exchangerate.host/list?access_key=#{ENV["ERKEY"]}"
 
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @er_key = ENV.fetch("ERKEY")
+  @api_url = "https://api.exchangerate.host/list?access_key=#{@er_key}"
+  @raw_er_data = HTTP.get(@api_url)
+  @raw_er_data_string = @raw_er_data.to_s
+  @parsed_er_data = JSON.parse(@raw_er_data_string)
+  @currencies_array = @parsed_er_data.fetch("currencies")
+  @currencies_keys = @currencies_array.keys
+  erb(:currencies)
+
 end
