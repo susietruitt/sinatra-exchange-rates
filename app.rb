51f5eb405827d33694de4b03e02c243a5d59d9b8
susietruitt <91689219+susietruitt@users.noreply.github.com>
2024-04-14 22:22:00 +0000

    final?

--- app.rb
+++ app.rb
@@ -18,3 +18,34 @@ get("/") do
   erb(:currencies)
 
 end
+
+get("/dynamic/:currency_one") do
+  @er_key = ENV.fetch("ERKEY")
+  @api_url = "https://api.exchangerate.host/list?access_key=#{@er_key}"
+  @raw_er_data = HTTP.get(@api_url)
+  @raw_er_data_string = @raw_er_data.to_s
+  @parsed_er_data = JSON.parse(@raw_er_data_string)
+  @currencies_array = @parsed_er_data.fetch("currencies")
+  @currencies_keys = @currencies_array.keys
+  @currency_one = params.fetch("currency_one")
+  erb(:c_one)
+end
+
+get("/dynamic/:currency_one/:currency_two") do
+  @er_key = ENV.fetch("ERKEY")
+  @api_url = "https://api.exchangerate.host/list?access_key=#{@er_key}"
+  @raw_er_data = HTTP.get(@api_url)
+  @raw_er_data_string = @raw_er_data.to_s
+  @parsed_er_data = JSON.parse(@raw_er_data_string)
+  @currencies_array = @parsed_er_data.fetch("currencies")
+  @currencies_keys = @currencies_array.keys
+  @currency_one = params.fetch("currency_one")
+  @currency_two = params.fetch("currency_two")
+
+  @conversion_url="https://api.exchangerate.host/convert?access_key=#{@er_key}&from=#{@currency_one}&to=#{@currency_two}&amount=1"
+  @raw_convert_data = HTTP.get(@conversion_url)
+  @raw_convert_data_string = @raw_convert_data.to_s
+  @parsed_convert_data = JSON.parse(@raw_convert_data_string)
+  @result = @parsed_convert_data["result"]
+  erb(:c_two)
+end
